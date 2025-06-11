e2365213fefc00b4d622d9e0a256fdc6ca940b2a
Spoorthi <spoorthijain0004@gmail.com>
2025-06-11 11:41:29 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+API_KEY = '8e48fa366467b47c97288f76783b203a'  # Replace with real API key
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather = {}
+    if request.method == 'POST':
+        city = request.form['city']
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            weather = {
+                'city': city,
+                'temperature': data['main']['temp'],
+                'description': data['weather'][0]['description'],
+                'humidity': data['main']['humidity'],
+                'icon': data['weather'][0]['icon']  # NEW
+            }
+        else:
+            weather['error'] = 'City not found!'
+    return render_template('index.html', weather=weather)
+
+if __name__ == '__main__':
+    app.run(debug=True)
